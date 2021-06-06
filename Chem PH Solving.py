bfe7f457822639704417e75e617605e778f1e075
maxbear123 <dmaxbear123@gmail.com>
2021-06-05 23:44:14 -0400

    - fixed spelling errors

--- Chem PH Solving.py
+++ Chem PH Solving.py
@@ -16,11 +16,11 @@
 import matplotlib.pyplot as plt
 import math
 
-# At the equivlance point (when the analyte has been completely neutralized), the pH of the solution is determined
+# At the equivalence point (when the analyte has been completely neutralized), the pH of the solution is determined
 # by the ionization of the salt in water
-# E.G. pH of CH3COOH + NaOH -> CH3COO- Na+ + HOH @ equivlance is determined by:
+# E.G. pH of CH3COOH + NaOH -> CH3COO- Na+ + HOH @ equivalence is determined by:
 # CH3COO- + HOH <--> CH3COOH + OH-  (here Kb=[CH3COOH][OH-]/[CH3COO-] and Kb is calculated from the Ka of CH3COOH)
-# Or HCl + NaOH -> NaCl + HOH @ equivlance is pH=7 (NaCl is strong base / strong acid; therefore neutral)
+# Or HCl + NaOH -> NaCl + HOH @ equivalence is pH=7 (NaCl is strong base / strong acid; therefore neutral)
 
 log = lambda x: math.log(x, 10)
 switch = lambda k: 10 ** (-(14 - p(k)))
@@ -30,7 +30,7 @@ n_quad = lambda a, b, c: ((-b - (b * b - 4 * a * c) ** 0.5) / (2 * a))
 
 
 # --------------react----------------------
-# Solves Stoich neutralizations in the form: (Can be used for any reaction with 2 reactants and 1-2 products)
+# Solves Stoichiometric neutralizations in the form: (Can be used for any reaction with 2 reactants and 1-2 products)
 # Analyte + Titrant --> Salt + Water
 # Or 
 # Analyte + Titrant --> Salt
@@ -42,8 +42,8 @@ n_quad = lambda a, b, c: ((-b - (b * b - 4 * a * c) ** 0.5) / (2 * a))
 # V2 -> Volume of Titrant
 # Unit -> "mL" or "L"
 # Returns {'analyte': mols of Analyte remaining, 'titrant': mols of Titrant remaining, 'salt': mols of Salt remaining,
-# 'V': volume of solution in L, 'titrant_mol_needed': moles of Titrant needed to reach equivlance pt,
-# 'titrant_mol_needed': volume (in L) of Titrant needed to reach equivlance pt}
+# 'V': volume of solution in L, 'titrant_mol_needed': moles of Titrant needed to reach equivalence pt,
+# 'titrant_mol_needed': volume (in L) of Titrant needed to reach equivalence pt}
 def react(ratio=[1, 1, 1, 1], C1=5, C2=5, V1=50, V2=25, unit='mL'):
     if unit == 'mL':
         mol1 = (V1 / 1000) * C1
@@ -120,7 +120,7 @@ def weak_pH(acid_or_base, k, conc):
 
 # TAKES CONCENTRATION, NOT MOLES
 # acid_or_base is for analyte 
-def equivlance_pH(acid_or_base, k, salt_conc):
+def equivalence_pH(acid_or_base, k, salt_conc):
     # weak_pH but convert K to opposite
     if acid_or_base == 'acid':
         return weak_pH('base', switch(k), salt_conc)
@@ -130,8 +130,8 @@ def equivlance_pH(acid_or_base, k, salt_conc):
 
 # rxn_mol -> output of react(), or:
 #      {'analyte': mols of Analyte remaining, 'titrant': mols of Titrant remaining, 'salt': mols of Salt remaining,
-# 'V': volume of solution in L, 'titrant_mol_needed': moles of Titrant needed to reach equivlance pt,
-# 'titrant_mol_needed': volume (in L) of Titrant needed to reach equivlance pt}
+# 'V': volume of solution in L, 'titrant_mol_needed': moles of Titrant needed to reach equivalence pt,
+# 'titrant_mol_needed': volume (in L) of Titrant needed to reach equivalence pt}
 # Ka/Kb -> constant for analyte (0 for the one that does not apply) (unless strong analyte)
 # acid_or_base is for analyte ('acid' or 'base')
 # if strong analyte - weak titrant, k is the k of the titrant
@@ -142,16 +142,16 @@ def get_pH(rxn_mol, k=1.7e-5, acid_or_base='acid', strong_titrant=True, strong_a
         # initial pH
         if rxn_mol['analyte'] > 0 and rxn_mol['titrant'] == 0 and rxn_mol['salt'] == 0:
             return weak_pH(acid_or_base, k, rxn_mol['analyte'] / rxn_mol['V'])
-        # before equivlance pt (buffer solution)
+        # before equivalence pt (buffer solution)
         if rxn_mol['analyte'] > 0 and rxn_mol['titrant'] == 0:
             if acid_or_base == 'acid':
                 return buffer_pH(acid_or_base, k, rxn_mol['analyte'], rxn_mol['salt'])
             if acid_or_base == 'base':
                 return buffer_pH(acid_or_base, k, rxn_mol['analyte'], rxn_mol['salt'])
-        # @ equivlance pt
+        # @ equivalence pt
         if rxn_mol['analyte'] == 0 and rxn_mol['titrant'] == 0:
-            return equivlance_pH(acid_or_base, k, (rxn_mol['salt'] / rxn_mol['V']))
-        # past equivlance pt (strong acid/base for this combo)
+            return equivalence_pH(acid_or_base, k, (rxn_mol['salt'] / rxn_mol['V']))
+        # past equivalence pt (strong acid/base for this combo)
         if rxn_mol['titrant'] > 0 and rxn_mol['analyte'] == 0:
             if acid_or_base == 'acid':
                 return strong_pH('base', rxn_mol['titrant'] / rxn_mol['V'])
@@ -161,13 +161,13 @@ def get_pH(rxn_mol, k=1.7e-5, acid_or_base='acid', strong_titrant=True, strong_a
         # initial pH
         if rxn_mol['analyte'] > 0 and rxn_mol['titrant'] == 0 and rxn_mol['salt'] == 0:
             return strong_pH(acid_or_base, rxn_mol['analyte'] / rxn_mol['V'])
-        # before equivlance pt (buffer solution)
+        # before equivalence pt (buffer solution)
         if rxn_mol['analyte'] > 0 and rxn_mol['titrant'] == 0:
             return strong_pH(acid_or_base, rxn_mol['analyte'] / rxn_mol['V'])
-        # @ equivlance pt
+        # @ equivalence pt
         if rxn_mol['analyte'] == 0 and rxn_mol['titrant'] == 0:
             return 7.0
-        # past equivlance pt (strong acid/base for this combo)
+        # past equivalence pt (strong acid/base for this combo)
         if rxn_mol['titrant'] > 0 and rxn_mol['analyte'] == 0:
             if acid_or_base == 'acid':
                 return weak_pH('base', k, rxn_mol['titrant'] / rxn_mol['V'])
@@ -177,44 +177,44 @@ def get_pH(rxn_mol, k=1.7e-5, acid_or_base='acid', strong_titrant=True, strong_a
         # initial pH
         if rxn_mol['analyte'] > 0 and rxn_mol['titrant'] == 0 and rxn_mol['salt'] == 0:
             return strong_pH(acid_or_base, rxn_mol['analyte'] / rxn_mol['V'])
-        # before equivlance pt (buffer solution)
+        # before equivalence pt (buffer solution)
         if rxn_mol['analyte'] > 0 and rxn_mol['titrant'] == 0:
             return strong_pH(acid_or_base, rxn_mol['analyte'] / rxn_mol['V'])
-        # @ equivlance pt
+        # @ equivalence pt
         if rxn_mol['analyte'] == 0 and rxn_mol['titrant'] == 0:
             return 7.0
-        # past equivlance pt (strong acid/base for this combo)
+        # past equivalence pt (strong acid/base for this combo)
         if rxn_mol['titrant'] > 0 and rxn_mol['analyte'] == 0:
             if acid_or_base == 'acid':
                 return strong_pH('base', rxn_mol['titrant'] / rxn_mol['V'])
             if acid_or_base == 'base':
                 return strong_pH('acid', rxn_mol['titrant'] / rxn_mol['V'])
     elif not (strong_analyte) and not (strong_titrant):
-        print('Weak-Weak titrations have not been implimented yet. They are not good experimental design.')
+        print('Weak-Weak titrations have not been implemented yet. They are not good experimental design.')
 
 
 #         # initial pH
 #         if rxn_mol['analyte']>0 and rxn_mol['titrant']==0 and rxn_mol['salt']==0:
 #             return weak_pH(acid_or_base,k,rxn_mol['analyte']/rxn_mol['V'])
-#         # before equivlance pt (buffer solution)
+#         # before equivalence pt (buffer solution)
 #         if rxn_mol['analyte']>0 and rxn_mol['titrant']==0:
 #             if acid_or_base=='acid':
 #                 return buffer_pH(acid_or_base,k,rxn_mol['analyte'],rxn_mol['salt'])
 #             if acid_or_base=='base':
 #                 return buffer_pH(acid_or_base,k,rxn_mol['analyte'],rxn_mol['salt'])
-#         # @ equivlance pt
+#         # @ equivalence pt
 #         if rxn_mol['analyte']==0 and rxn_mol['titrant']==0:
 #             return 7.0
-#         # past equivlance pt (strong acid/base for this combo)
+#         # past equivalence pt (strong acid/base for this combo)
 #         if rxn_mol['titrant']>0 and rxn_mol['analyte']==0:
 #             if acid_or_base=='acid':
 #                 return strong_pH('base',rxn_mol['titrant']/rxn_mol['V'])
 #             if acid_or_base=='base':
 #                 return strong_pH('acid',rxn_mol['titrant']/rxn_mol['V'])
-# equivlance point of strong-strong if 1:1:1:1 is always pH=7.00
+# equivalence point of strong-strong if 1:1:1:1 is always pH=7.00
 # weak-weak is not a good experimental design
 
-# final_vol is max ammount added 
+# final_vol is max amount added
 # if strong analyte - weak titrant, k is the k of the titrant
 # k2 is only for weak-weak titrations, where it is the k of the titrant 
 def plot_titration(initial_vol=0, final_vol=100, increment=0.1, ratio=[1, 1, 1, 1], C1=5, C2=5, V1=50, V2=25, unit='mL',
@@ -243,16 +243,17 @@ def plot_titration(initial_vol=0, final_vol=100, increment=0.1, ratio=[1, 1, 1, 
     if not (strong_analyte) and strong_titrant:
         salt_conc_at_eqvl = react(ratio=ratio, C1=C1, C2=C2, V1=V1, V2=tit_vol_needed, unit='mL')['salt'] / \
                             react(ratio=ratio, C1=C1, C2=C2, V1=V1, V2=tit_vol_needed, unit='mL')['V']
-        equ_pH = equivlance_pH(acid_or_base, k, salt_conc_at_eqvl)
+        equ_pH = equivalence_pH(acid_or_base, k, salt_conc_at_eqvl)
     elif (strong_analyte and not (strong_titrant)) or (strong_analyte and strong_titrant):
         equ_pH = 7.0
     print('Initial pH: ' + str(y[0]))
-    print('pH at Equivlance Point: ' + str(equ_pH))
+    print('pH at Equivalence Point: ' + str(equ_pH))
     print('Final pH:', str(y[len(y) - 1]))
-    print('Volume of Titrant Needed for Equivlance:', str(tit_vol_needed), 'mL')
-    print('Ammount of Titrant Needed for Equivlance:', str(tit_mol_needed), 'mol')
+    print('Volume of Titrant Needed for Equivalence:', str(tit_vol_needed), 'mL')
+    print('Amount of Titrant Needed for Equivalence:', str(tit_mol_needed), 'mol')
     plt.show()
     return x, y
 
-
+# just the default example
+# notice the vertical line around equivalence; this is not accruate and should be ignored
 plot_titration()
